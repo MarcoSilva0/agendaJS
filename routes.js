937a73db7e99239ed77eb0fae24ff32e01ac99cf
MarcoSilva0 <marcoantonio02016@outlook.com>
2022-10-16 10:49:49 -0300

    Inserido a parte de adição de contato e visualização do meus
    Signed-off-by: MarcoSilva0 <marcoantonio02016@outlook.com>

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const {loginRequired} = require('./src/middlewares/middleware');
 
 //Rotas da home
 route.get('/', homeController.index);
@@ -13,6 +16,10 @@ route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
+//Rotas de Contato
+route.get('/contato', loginRequired,contatoController.index);
+route.get('/contato/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/register', loginRequired, contatoController.register);
 
 //Exportação de todas as rotas
 module.exports = route;
